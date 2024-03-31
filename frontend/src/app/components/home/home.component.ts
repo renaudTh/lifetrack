eab05dee15f4ee723a9e4733edaed737fab3efa0
Thomas Renaud <t.renaud@neuf.fr>
2024-03-31 10:41:25 +0200

    wip : style the calendar

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -2,8 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { ACTIVITY_PROVIDER, IActivityProvider } from '../../../domain/activity.provider.interface';
 import { ActivityMinimalComponent } from '../activity-minimal/activity-minimal.component';
-
-
+import { ButtonModule } from 'primeng/button';
 
 export interface Day {
   number: number;
@@ -15,7 +14,7 @@ export interface Day {
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ActivityMinimalComponent],
+  imports: [CommonModule, ActivityMinimalComponent, ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -23,6 +22,7 @@ export class HomeComponent {
 
   private currentDate = new Date();
   private displayedDate = new Date();
+  public selected: Day | null = null;
 
   get title() {
     return this.displayedDate.toLocaleDateString('en-US', { month: 'long', year: "numeric"});
@@ -31,6 +31,11 @@ export class HomeComponent {
   get dayList(){
     const list = this.generateDaysOfMonth(this.displayedDate);
     return list;
+  }
+
+  getClassList(day: Day){
+    const isSelected = JSON.stringify(day) === JSON.stringify(this.selected);
+    return [!day.inCurrentMonth ? 'not-current-month' : '', day.currentDate ? 'current-date' : '', isSelected ? 'selected' : ''];
   }
   constructor(@Inject(ACTIVITY_PROVIDER) private activityProvider: IActivityProvider){
     this.displayedDate.setDate(1);
@@ -45,12 +50,29 @@ export class HomeComponent {
     // console.log(this.displayedDate.toLocaleDateString());
   }
   next(){
+    
     const month = this.displayedDate.getMonth()
     this.displayedDate.setMonth(month + 1); 
     // console.log(this.displayedDate.toLocaleDateString());
   }
   isToday(date: Date){
     return date.toDateString() === new Date().toDateString();
+  }
+  get canNext(): boolean {
+    
+    const displayedMonth = this.displayedDate.getMonth();
+    const displayedYear = this.displayedDate.getFullYear();
+    const currentMonth = this.currentDate.getMonth();
+    const currentYear = this.currentDate.getFullYear();
+    if(displayedYear < currentYear) return true;
+    if(displayedYear === currentYear){
+      return (displayedMonth + 1) <= currentMonth;
+    }
+    return false;
+  }
+  selectDay(day: Day){
+    this.selected = day;
+    console.log("selected: ", this.selected);
   }
   generateDaysOfMonth(date: Date): Day[] {
 
