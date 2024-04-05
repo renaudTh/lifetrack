59ed586e0770aa4171a91b6e3234510f746f96eb
Thomas Renaud <t.renaud@neuf.fr>
2024-04-05 23:43:16 +0200

    feat: simplify calendar using date service

--- frontend/src/app/components/calendar/calendar.component.ts
+++ frontend/src/app/components/calendar/calendar.component.ts
@@ -1,11 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
-export interface Day {
-  date: Date;
-  inCurrentMonth: boolean;
-  currentDate?: boolean;
-}
+import { DateService } from '../../../domain/date.service';
+import { Day } from '../../../domain/date';
 
 @Component({
   selector: 'app-calendar',
@@ -15,87 +12,42 @@ export interface Day {
   styleUrl: './calendar.component.scss'
 })
 export class CalendarComponent {
-  private currentDate = new Date();
-  private displayedDate = new Date();
-  public selectedDate: Date | null = null;
-  
-  @Output() onSelect = new EventEmitter<Date>();
 
 
-  get title() {
-    return this.displayedDate.toLocaleDateString('en-US', { month: 'long', year: "numeric"});
-  }
 
-  get dayList(){
-    const list = this.generateDaysOfMonth(this.displayedDate);
-    return list;
-  }
 
   getClassList(day: Day){
-    const isSelected = day.date.toLocaleDateString() === this.selectedDate?.toLocaleDateString()
-    return [!day.inCurrentMonth ? 'not-current-month' : '', day.currentDate ? 'current-date' : '', isSelected ? 'selected' : ''];
+    return [!day.inCurrentMonth ? 'not-current-month' : '', day.currentDate ? 'current-date' : '', day.selected ? 'selected' : ''];
   }
-  constructor(){
-    this.displayedDate.setDate(1);
-    this.displayedDate.setMonth(this.displayedDate.getMonth());
-    this.displayedDate.setFullYear(this.currentDate.getFullYear());
 
-    this.selectedDate = new Date();
+  constructor(private dateService: DateService){
   }
+  
+  protected displayedDateString$ = this.dateService.displayedDateString$;
+  protected daysOfMoth$ = this.dateService.daysOfCurrentMonth$;
 
   previous(){
-    const month = this.displayedDate.getMonth()
-    this.displayedDate.setMonth(month - 1);
+    this.dateService.previousMonth();
   }
   next(){
-    
-    const month = this.displayedDate.getMonth()
-    this.displayedDate.setMonth(month + 1); 
+    this.dateService.nextMonth();
   }
-  isToday(date: Date){
-    return date.toDateString() === new Date().toDateString();
+  selectDay(day: Date){
+    this.dateService.selectedDate = day;
   }
   get canNext(): boolean {
     
-    const displayedMonth = this.displayedDate.getMonth();
-    const displayedYear = this.displayedDate.getFullYear();
-    const currentMonth = this.currentDate.getMonth();
-    const currentYear = this.currentDate.getFullYear();
-    if(displayedYear < currentYear) return true;
-    if(displayedYear === currentYear){
-      return (displayedMonth + 1) <= currentMonth;
-    }
-    return false;
+    // const displayedMonth = this.displayedDate.getMonth();
+    // const displayedYear = this.displayedDate.getFullYear();
+    // const currentMonth = this.currentDate.getMonth();
+    // const currentYear = this.currentDate.getFullYear();
+    // if(displayedYear < currentYear) return true;
+    // if(displayedYear === currentYear){
+    //   return (displayedMonth + 1) <= currentMonth;
+    // }
+    return true;
   }
-  selectDay(date: Date){
-    this.selectedDate = date;
-    this.onSelect.emit(date)
-  }
+ 
 
-  generateDaysOfMonth(date: Date): Day[] {
-
-    const result: Day[] = [];
-    let _date = new Date(date);
-
-    // We want to begin on Monday
-    while(_date.getDay() !== 1){
-      const dayNumber = _date.getDate(); 
-        _date.setDate(dayNumber - 1);
-        result.unshift({date: new Date(_date), inCurrentMonth: false, currentDate:this.isToday(_date)  });
-      }
-      // Reset to the beginning of the month
-     _date = new Date(date);
-      while (_date.getMonth() === date.getMonth()) {
-      const dayNumber = _date.getDate();
-      result.push({ date: new Date(_date), inCurrentMonth: true, currentDate:this.isToday(_date) });
-      _date.setDate(dayNumber + 1);
-    }
-    //We want to end on sunday
-    while (_date.getDay() !== 1) {
-      const dayNumber = _date.getDate();
-      result.push({ date: new Date(_date), inCurrentMonth: false, currentDate:this.isToday(_date) });
-      _date.setDate(dayNumber + 1);
-    }
-    return result;
-  }
+  
 }
