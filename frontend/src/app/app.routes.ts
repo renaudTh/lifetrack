3c113ebef7d3d9153726aa46c9028e7ebf0cff30
Thomas Renaud <t.renaud@neuf.fr>
2024-12-15 23:13:37 +0100

    feat: add login route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthGuard, LoginGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     {
         path: "",
-        component: HomeComponent
+        component: HomeComponent,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: "login",
+        component: LoginComponent,
+        canActivate: [
+           LoginGuard
+        ]
     }
 ];
