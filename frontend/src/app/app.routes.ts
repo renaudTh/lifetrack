22139e1c9ef2c5299a5896d27948dabfa8611fd9
Thomas Renaud <t.renaud@neuf.fr>
2024-12-22 14:34:03 +0100

    feat: add stats route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,17 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard, LoginGuard } from './guards/auth.guard';
+import { StatisticsComponent } from './components/statistics/statistics.component';
 
 export const routes: Routes = [
     {
         path: "",
         component: HomeComponent,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: "statistics",
+        component: StatisticsComponent,
         canActivate: [AuthGuard]
     },
     {
