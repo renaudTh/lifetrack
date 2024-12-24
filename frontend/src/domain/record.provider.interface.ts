54be1677384cb0751e3204875c742ec39ef933df
Thomas Renaud <t.renaud@neuf.fr>
2024-12-24 09:23:55 +0100

    feat: factorize method to get records between dates

--- frontend/src/domain/record.provider.interface.ts
+++ frontend/src/domain/record.provider.interface.ts
@@ -1,7 +1,8 @@
 import { InjectionToken } from "@angular/core";
 import { Observable } from "rxjs";
-import { ActivityRecord } from "./activity";
 import { RecordDto } from "../providers/record.dto";
+import { ActivityRecord } from "./activity";
+import { DjsDate } from "./date";
 
 export const RECORD_PROVIDER = new InjectionToken<IRecordProvider>("record.provider.interface");
 
@@ -10,4 +11,5 @@ export interface IRecordProvider {
     upsertRecord(record: RecordDto): Observable<ActivityRecord>
     getUserMonthHistory(date: DjsDate): Observable<ActivityRecord[]>
     downsertRecord(record: ActivityRecord): Observable<ActivityRecord>
+    getRecordsBetweenDates(start: DjsDate, end: DjsDate): Observable<ActivityRecord[]>
 }
